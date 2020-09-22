26bc0af164dcd6c959fceac13912e9849df7d910
amanchowdhury046 <70589402+amanchowdhury046@users.noreply.github.com>
2020-09-22 20:56:57 +0530

    Create GenerateSubarray.cpp

---
+++ GenerateSubarray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+void generate_subarray(int a[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i; j < n; j++)
+        {
+            for (int k = i; k <= j; k++)
+            {
+                cout << a[k] << ", ";
+            }
+            cout << endl;
+        }
+    }
+}
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    generate_subarray(a, n);
+    return 0;
+}
